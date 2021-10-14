9f23ac121ba3bf307ac38ce2517fb6cc631121f5
Rodal Issema <misterrodalissema@gmail.com>
2021-10-14 01:40:11 -0700

    First commit

---
+++ init.el
@@ -0,0 +1,35 @@
+(require 'package)
+
+
+(when (fboundp 'electric-indent-mode) (electric-indent-mode -1)) ;; disable orgmode auto indent
+
+(setq package-enable-at-statup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(unless (package-installed-p 'dracula-package) ;;enabling dracule theme
+  (package-refresh-contents)
+  (package-install 'dracula-theme))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(dracula))
+ '(custom-safe-themes
+   '("0ab2aa38f12640ecde12e01c4221d24f034807929c1f859cbca444f7b0a98b3a" default))
+ '(package-selected-packages '(beacon dracula-theme which-key use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 125 :width normal :foundry "ADBO" :family "Source Code Pro")))))
